a417bcaa470da974c259ba7cf5a808b9a0c28366
home <livanovandrej99@gmail.com>
2023-01-24 18:52:09 +0700

    search and pagination

---
+++ src/pages/Home.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import PizzaBlock from '../components/PizzaBlock';
+import Pagination from '../components/Pagination';
+
+const Home = ({ searchValue }) => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности (ASC)',
+    sortProperty: 'rating',
+  });
+
+  React.useEffect(() => {
+    setIsLoading(true);
+
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const order = sortType.sortProperty.includes('-') ? 'desc' : 'asc';
+
+    fetch(
+      `https://63c51c19e1292e5bea1af867.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+    )
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories
+          value={categoryId}
+          onChangeCategory={(i) => setCategoryId(i)}
+        />
+        <Sort value={sortType} onChangeSort={(obj) => setSortType(obj)} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6).keys()].map((value) => <Skeleton key={value} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
+    </div>
+  );
+};
+
+export default Home;
